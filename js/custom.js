40401a93be0e472e1e7447fb01abc9c760ff0506
HoldenGrant <holdengrantcausing@gmail.com>
2016-07-20 21:49:27 +0800

    asd

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+//    smooth scrolling
+$('a[href^=#]').on("click",function(){
+    var t= $(this.hash);
+    var t=t.length&&t||$('[name='+this.hash.slice(1)+']');
+    if(t.length){
+        var tOffset=t.offset().top;
+        $('html,body').animate({scrollTop:tOffset-20},'slow');
+        return false;
+    }
+});
+// change on scroll
+$(document).ready(function(){
+    var scroll_start = 0;
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
+    if (startchange.length){
+       $(document).scroll(function() {
+       scroll_start = $(this).scrollTop();
+            if(scroll_start > offset.top) {
+                $(".main-menu-container").addClass('top-menu-container');
+                $('.navbar-default .navbar-brand').css('display', 'block');
+            } else {
+                $(".main-menu-container").removeClass('top-menu-container');
+                $('.navbar-default .navbar-brand').css('display', 'none');
+
+            }
+       });
+    }
+});
